f42e2c0c996bdc933b240abe938e875930d42245
Navya Annapareddy <navya@d-172-26-40-239.dhcp.virginia.edu>
2019-10-20 13:26:18 -0400

    Updated template

--- app/cms/records/models.py
+++ app/cms/records/models.py
@@ -3,14 +3,14 @@ from django.db import models
 class Doctor(models.Model):
     doc_first = models.CharField(max_length=30)
     doc_last  = models.CharField(max_length=30)
-    ID = models.IntegerField()
+#    ID = models.IntegerField()
 
 class Patient(models.Model):
     patient_first = models.CharField(max_length=30)
     patient_last = models.CharField(max_length=30)
     ethnicity = models.CharField(max_length=30)
     AGE = models.IntegerField()
-    MEID = models.IntegerField()
+#    MEID = models.IntegerField()
 
 class Contact(models.Model):
     contact_first = models.CharField(max_length=30)
