67b85b1fbd233f6afb05dd48f180fd0d558aff95
Navya Annapareddy <navya@Navyas-MacBook-Pro.local>
2019-10-27 15:46:21 -0400

    Added template inheritance

--- app/cms/records/models.py
+++ app/cms/records/models.py
@@ -1,21 +1,26 @@
 from django.db import models
 
 class It(models.Model):
-    ip_last = models.CharField(max_length=30)
-    ip_first = models.CharField(max_length=30)
-    ip_MRN = models.IntegerField()
-    ip_cg = models.IntegerField()
-    ip_tg = models.IntegerField()
+    ip_last = models.CharField(max_length=30, blank=True, default='')
+    ip_first = models.CharField(max_length=30, blank=True,  default='')
+    ip_MRN = models.IntegerField(blank=True,  default=0)
+    ip_cg = models.IntegerField(blank=True,  default=100)
+    ip_tg = models.IntegerField(blank=True,  default=180)
+    def intro(self):
+        return "Patient {} {}".format(self.ip_first, self.ip_last)
 
 class Nit(models.Model):
-    p_last = models.CharField(max_length=30)
-    p_first = models.CharField(max_length=30)
-    p_MRN = models.IntegerField()
-    p_cg = models.IntegerField()
-    p_tg = models.IntegerField()
+    p_last = models.CharField(max_length=30, blank=True,  default='')
+    p_first = models.CharField(max_length=30, blank=True,  default='')
+    p_MRN = models.IntegerField(blank=True, default=0)
+    p_cg = models.IntegerField(blank=True, default=100)
+    p_tg = models.IntegerField(blank=True, default=180)
+    def stat(self):
+        return "Patient {} has stable BG of {}".format(self.p_MRN, self.p_cg)
 
 class Nurses(models.Model):
-    n_last = models.CharField(max_length=30)
-    n_first = models.CharField(max_length=30)
-    n_UNIT = models.IntegerField()
-
+    n_last = models.CharField(max_length=30, blank=True,  default='')
+    n_first = models.CharField(max_length=30, blank=True,  default='')
+    n_UNIT = models.IntegerField(blank=True, default=1)
+    def assign(self):
+        return "Nurse {} is staffing Unit {}".format(self.n_last, self.n_UNIT)
--- app/cms/records/tests.py
+++ app/cms/records/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from records.models import It, Nit, Nurses
 
-# Create your tests here.
+class ItTestCase(TestCase):
+    def setUp(self):
+        It.objects.create(ip_first="Martha", ip_last="Jefferson")
+        It.objects.create(ip_first="Thomas", ip_last="Jefferson")
+
+    def test_it_introduction(self):
+        it1 = It.objects.get(ip_first = "Thomas")
+        it2 = It.objects.get(ip_first = "Martha")
+        self.assertEqual(it1.intro(), "Patient Thomas Jefferson")
+        self.assertEqual(it2.intro(), "Patient Martha Jefferson")
+
+class NitTestCase(TestCase):
+    def setUp(self):
+        Nit.objects.create(p_MRN=123, p_cg=180)
+        Nit.objects.create(p_MRN=321, p_cg=170)
+
+    def test_nit_introduction(self):
+        nit1 = Nit.objects.get(p_MRN=123, p_cg=180)
+        nit2 = Nit.objects.get(p_MRN=321, p_cg=170)
+        self.assertEqual(nit1.stat(), "Patient 123 has stable BG of 180")
+        self.assertEqual(nit2.stat(), "Patient 321 has stable BG of 170")
+
+class NursesTestCase(TestCase):
+    def setUp(self):
+        Nurses.objects.create(n_last="Adams", n_UNIT=2)
+        Nurses.objects.create(n_last="Hamilton", n_UNIT=3)
+
+    def test_nurses_introduction(self):
+        n1 = Nurses.objects.get(n_last="Adams")
+        n2 = Nurses.objects.get(n_last = "Hamilton")
+        self.assertEqual(n1.assign(), "Nurse Adams is staffing Unit 2")
+        self.assertEqual(n2.assign(), "Nurse Hamilton is staffing Unit 3")
